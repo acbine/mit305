d1fa4789a13834a6cb0bb0eeebfd55725e9ddce8
Test <b1gdd@naver.com>
2023-12-08 17:32:40 +0900

    <feat> 깃생성

---
+++ src/main/java/com/example/tae/TaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaeApplication.class, args);
+    }
+
+}

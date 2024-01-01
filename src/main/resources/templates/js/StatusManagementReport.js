77911989c855f5400495ff3381d999baecbe0a0c
1arged <94172354+1argeD@users.noreply.github.com>
2024-01-02 00:08:27 +0900

    <feat> total html수정 및 exitence 랑 stckDelivery html 분리

--- src/main/resources/templates/js/StatusManagementReport.js
+++ src/main/resources/templates/js/StatusManagementReport.js
@@ -1,7 +1,8 @@
 
 google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
           ['미검수',     1,],
@@ -13,13 +14,12 @@ google.charts.load("current", {packages:["corechart"]});
         var options = {
           pieHole: 0.45,
           colors: ['#F20505', '#F28749', '#03A64A', '#8268A6'] // 색상 변경
-        
-          
+
+
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        
+
         chart.draw(data, options);
       }
 
-  

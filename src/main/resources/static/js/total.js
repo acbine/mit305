a7acf5044a79e091e9cf46b8c6eb23905f0d3ab3
chang uk <youngjjag28@gmail.com>
2024-01-02 14:41:23 +0900

    <chore> total.js 수정 - 품목정보등록 함수 반영

--- src/main/resources/static/js/total.js
+++ src/main/resources/static/js/total.js
@@ -1,3 +1,129 @@
+/*-------------------품목 정보(ProductInformationRegistration)-------------------------*/
+  function info_modify_and_save(tag) {
+
+        const tr = tag.closest("tr");
+
+        const cell_0 = tr.children[0];
+        const cell_1 = tr.children[1];
+        const cell_2 = tr.children[2];
+        const cell_3 = tr.children[3];
+        const cell_4 = tr.children[4];
+        const cell_5 = tr.children[5];
+        const cell_6 = tr.children[6];
+
+
+        if (cell_0.contentEditable == "true") {
+
+            cell_0.contentEditable = "false";
+            cell_1.contentEditable = "false";
+            cell_2.contentEditable = "false";
+            cell_3.contentEditable = "false";
+            cell_4.contentEditable = "false";
+            cell_5.contentEditable = "false";
+            cell_6.contentEditable = "false";
+
+            tag.innerText = "수정";
+
+        //첫번째 셀의 contenteditable 속성이 false라면(나머지 셀들의 속성 동일)
+        } else {
+
+            //각 셀들의 contenteditable 속성 true로 모두 변경하여 수정 가능하게 함
+
+            cell_0.contentEditable = "true";
+            cell_1.contentEditable = "true";
+            cell_2.contentEditable = "true";
+            cell_3.contentEditable = "true";
+            cell_4.contentEditable = "true";
+            cell_5.contentEditable = "true";
+            cell_6.contentEditable = "true";
+
+            tag.innerText = "수정 완료";
+
+            cell_0.focus();
+        }
+    }
+
+    function info_registration_and_delete(tag) {
+
+        const tr = tag.closest("tr");
+        tr.remove();
+    }
+
+
+    function info_addRow() {
+
+        const table = document.getElementById('product');
+        const new_row = table.insertRow();
+
+        const cell_length = table.rows[1].cells.length;
+
+        for(let i = 0; i < cell_length; i++) {
+            const new_cell = new_row.insertCell(i);
+            let temp_html = '';
+
+            if(i == 7) {
+
+                temp_html =
+                    '<td>' +
+                        '<select name="state">' +
+                            '<option value="first">대분류 1</option>' +
+                            '<option value="second">대분류 2</option>' +
+                        '</select>' +
+                    '</td>';
+            }
+
+            if(i == 8) {
+
+                temp_html =
+                    '<td>' +
+                        '<select name="state">' +
+                            '<option value="first">중분류 1</option>' +
+                            '<option value="second">중분류 2</option>' +
+                        '</select>' +
+                    '</td>';
+            }
+
+            if(i == 9) {
+
+                temp_html =
+                    '<td>' +
+                        '<select name="state">' +
+                            '<option value="first">소분류 1</option>' +
+                            '<option value="second">소분류 2</option>' +
+                        '</select>' +
+                    '</td>';
+            }
+
+            if(i == 10) {
+
+                temp_html = '<td>'+
+                               '<input type="file" accept="image/png, image/jpg">' +
+                               '</td>';
+            }
+
+            if(i == 11) {
+
+                temp_html = '<td>계약 안됨</td>'
+            }
+
+            if(i == 12) {
+
+                temp_html =
+                    '<td>' +
+                        '<div class="actions">' +
+                            '<button class="action-button action-button-edit" onclick="info_modify_and_save(this)">수정</button>' +
+                            '<button class="action-button action-button-delete" onclick="info_registration_and_delete(this)">삭제</button>' +
+                        '</div>' +
+                    '</td>';
+            }
+
+             new_cell.insertAdjacentHTML('afterbegin', temp_html);
+
+        }
+
+
+    }
+
 /*--------------------발주 목록(orderList)--------------------*/
 function openPopup(popupId) {
     var popup = document.getElementById(popupId);
@@ -85,6 +211,7 @@ function printTradingStatement() {
     window.print();
     $(".TradingmyModal").css('display', 'none');
 }
+
 
 /*---------------------계약등록-------------------*/
 

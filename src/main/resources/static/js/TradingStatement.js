f72ea2241cfb59be7015d82d0151552763947e2b
bin <kiia8858@naver.com>
2024-01-05 17:03:46 +0900

    <chore> 거래명세서 스크립트 수정

--- src/main/resources/static/js/TradingStatement.js
+++ src/main/resources/static/js/TradingStatement.js
@@ -1,16 +1,11 @@
+function Tradingmy(){  //모달창열기
+    console.log("트레딩모달 버튼 클릭됨");
+    $(".TradingmyModal").css('display','block');
+}
 
-$(function Tradingmy(){  //모달창열기
-    $("#show").on("click",function(){
-        console.log("트레딩모달 버튼 클릭됨");
-        $(".TradingmyModal").css('display','block');  
-        
-    });
+function CloseTradingMy(){  //모달창열기
+    console.log("모달콘튼츠창클릭 백그라운드 클릭");
+    $(".TradingmyModal").css('display','none');
+}
 
-    $(".TradingmyModal").on('click',function(){
-        console.log("모달콘튼츠창클릭 백그라운드 클릭");
-        $(".TradingmyModal").css('display','none');
-    });
-    
-});
- 
 

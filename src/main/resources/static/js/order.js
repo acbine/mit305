322d25c7614486174461a467a123c5894566ab5b
Test <b1gdd@naver.com>
2024-01-29 09:27:03 +0900

    <chore>날짜로 발주서를 검색

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -15,13 +15,13 @@ function openOrderInspectPopup(productCode,procurementPlanCode){  //모달창열
 }
 function searchOrderListWithDate() {
     var date1 = document.getElementsByName("startDate")[0].value;
-    console.log(date1)
     var date2 = document.getElementsByName("endDate")[0].value;
     $.ajax({
             url: 'order-list-with-date?&date1=' + date1 +'&date2='+date2,
             method:'get',
         success:function (info){
-
+                console.log(info,"info 정보 확인하기")
+                drawHTMl(info)
                 console.log("성공");
         },
         error:function (){
@@ -32,6 +32,21 @@ function searchOrderListWithDate() {
         )
 }
 
+
+function drawHTMl(info) {
+    var orderBoxInfo = document.getElementsByClassName("orderList")
+    for(var i = 0 ; i<orderBoxInfo.length; i++) {
+        orderBoxInfo[i].innerHTML = `<td>${info.productName}</td>
+                     
+                                    <td>${info.productName}</td>
+                                    <td>${info.productName}</td>
+                                    <td>${info.productName}</td>
+                                    <td>${info.productName}</td>v
+                                    <td>${info.productName}</td>
+`
+    }
+
+}
 /*--------------------발주 목록 팝업창(orderListPopup)--------------------*/
 function downloadImage(){
     /*html2canvas(document.getElementById('screen_area'),{scale:2}).then((canvas) => {

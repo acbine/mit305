a3fd0e97adffed2348e5dfeda0f9f9b095331adb
chang uk <youngjjag28@gmail.com>
2024-01-29 16:54:31 +0900

    <chore> 품목 수정/삭제, 이메일 전송/ 조달 계획 검색 범위 수정 완료

--- src/main/java/com/example/tae/repository/ProductForProjectRepository.java
+++ src/main/java/com/example/tae/repository/ProductForProjectRepository.java
@@ -2,14 +2,13 @@ package com.example.tae.repository;
 
 import com.example.tae.entity.ProductForProject.ProductForProject;
 import com.example.tae.entity.ProductForProject.ProductForProjectEmbeddable;
-import com.example.tae.entity.ProductInformation.ProductInformationRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface ProductForProjectRepository extends JpaRepository <ProductForProject,ProductForProjectEmbeddable> {
+public interface ProductForProjectRepository extends JpaRepository <ProductForProject, ProductForProjectEmbeddable> {
 
     @Query("select pf from ProductForProject    pf" +
         " where pf.productCode.product_code = :productId ")
